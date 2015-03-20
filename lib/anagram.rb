5c1ac05b8d9fb0f500b0ce642e0b894706334e42
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-03-20 12:48:30 +0000

    Gus solved

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,7 @@
 #Your code goes here
+
+def find_anagrams(letters, word_list)
+  word_list.select { |word| word.chars.sort == letters.chars.sort }
+end
+
+p find_anagrams("cat", ["try","how","eat","far","act","see","hit","lie","key","tac","sit","its","tea"])
